c9c4c3d4f115939378622ad5fd3f33ba91191936
Hanjun Yoo <hjyu@bu.edu>
2025-07-08 09:08:33 -0400

    initial-commit

---
+++ src/main.tsx
@@ -0,0 +1,11 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.tsx'
+
+// Render our <App> in <StrictMode> inside the innerHTML of the element with id root.
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
